42ee3a57e6c9cfa49a7302ad5073572b3f5a7d12
niharikapasi <niharikapasi@gmail.com>
2018-06-17 20:58:10 +0200

    Update samplecleaning.py

--- samplecleaning.py
+++ samplecleaning.py
@@ -4,12 +4,28 @@ import pandas as pd
 import numpy as np
 from pymongo import MongoClient
 
-#connect to Database
+pd.set_option('display.height', 1000)
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+#---------connect to Database----------
+
 connection = MongoClient('localhost', 27017)
 db = connection.newsdata
 
-#handle newsdatac collection
+#----------handle newsdatac collection----------
+
 data = db.samplecleaning
 newsDataList = pd.DataFrame(list(data.find()))
-#newsDataList = newsDataList[['_id', 'publication','date','title','author','content']]
+newsDataList = newsDataList[['_id', 'publication','title','content','author','date','cleaned']]
+
+
+#---------start cleaning---------
+
+#set _id as unique identifier to refer each tuple
+newsDataList['_id'].is_unique
+newsDataList.set_index('_id', inplace=True)
+newsDataList.head()
+
 print(newsDataList)
